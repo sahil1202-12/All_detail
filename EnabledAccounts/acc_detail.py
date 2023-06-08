fb95290a8d608299a62b5b327ddeaf0839838d47
sahil1202-12 <78477505+sahil1202-12@users.noreply.github.com>
2023-06-08 10:49:09 +0530

    acc_detail.py

--- EnabledAccounts/acc_detail.py
+++ EnabledAccounts/acc_detail.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import os
 
 def get_account_details():
     client = boto3.client('organizations')
@@ -48,16 +49,27 @@ def get_account_details():
         # Add the account details to the list
         account_details.append(account_info)
 
-    # Specify the path to the output folder and file name
-    output_folder = '/home/sahil/Documents/All_detail/output/Account details'
-    output_file = f'{output_folder}/account_details.json'
+    # Find the output folder path
+    current_directory = os.getcwd()
+    output_folder = os.path.join(current_directory, 'output')
+
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Specify the output file path
+    account_details_folder = os.path.join(output_folder, 'Account details')
+    output_file = os.path.join(account_details_folder, 'account_details.json')
+
+    # Create the Account details folder if it doesn't exist
+    if not os.path.exists(account_details_folder):
+        os.makedirs(account_details_folder)
 
     # Write the account details to a JSON file in the specified folder
     with open(output_file, 'w') as file:
         json.dump(account_details, file, indent=4)
 
     print("JSON file generated successfully with account details.")
-
 
 
 
